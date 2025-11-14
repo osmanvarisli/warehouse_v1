9cea06d406eaa4f5a39dae2f6de3d7d438543f81
Osman VARIŞLI <64133166+osmanvarisli@users.noreply.github.com>
2025-11-14 23:38:30 +0300

    Add files via upload

---
+++ resources/lang/tr/auth.php
@@ -0,0 +1,25 @@
+<?php
+
+return [
+
+    /*
+    |--------------------------------------------------------------------------
+    | Kimlik Doğrulama
+    |--------------------------------------------------------------------------
+    |
+    | Aşağıdaki metinler kimlik doğrulama (giriş) sırasında kullanıcılara
+    | gösterilebilecek mesajlardır. Bu metinleri uygulamanızın
+    | gereksinimlerine göre düzenlemekte özgürsünüz.
+    |
+    */
+
+    'failed' => 'Girilmiş olan kullanıcı verileri sistemdekiler ile eşleşmemektedir.',
+    'password' => 'Belirtilen şifre yanlış.',
+    'throttle' => 'Çok fazla oturum açma girişiminde bulundunuz. Lütfen :seconds saniye sonra tekrar deneyiz.',
+    'Login'=>'Giriş',
+    'Email Address'=>'Mail Adresi',
+    'Password'=>'Şifre',
+    'Forgot Your Password?'=>'Şifreni mi unuttun?',
+    'Remember Me'=>'Beni Hatırla',
+
+];
