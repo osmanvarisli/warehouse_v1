36bfa30e17b080babc6524b691ddc89afda04fda
Osman VARIŞLI <64133166+osmanvarisli@users.noreply.github.com>
2026-03-18 15:54:14 +0300

    Update web.php

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,10 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+/*
+Admin ile ilgili .... yok. 
 
+*/
 
 
 Auth::routes();
